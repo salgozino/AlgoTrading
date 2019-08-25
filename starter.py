4ac52e82fb69acd87fbd9dfc9ad726944c605e2f
Santiago Algozino <salgozino@gmail.com>
2019-08-25 13:05:42 -0300

    Create starter function for the botMiron, for a cronjob

--- starter.py
+++ starter.py
@@ -1,16 +1,21 @@
 import botMiron
-import botFollowTheLeader
 from utils.createLogger import createLogger
 
 
 if __name__ == '__main__':
-
+    user = ''
+    password = ''
+    account = ''
+    db = 'remarkets.db'
+    entorno = 1
     logger = createLogger()
-    botMiron.run()
-    botFollowTheLeader.run()
+    botMiron.run(user=user, password=password, account=account, entorno=entorno,     db=db)
+    
 
-    while True:
+    while not botMiron.stopping.is_set():
         try:
             pass
         except KeyboardInterrupt:
-                logger.debug("Hasta la vista, baby!")
+            logger.debug("Closed by user")
+            break
+    logger.debug("Hasta la vista, baby!")
